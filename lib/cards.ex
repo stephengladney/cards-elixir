76e224c7abe9bb26fd43d90aa47e326c26a8d79f
Stephen Gladney <stephengladney@gmail.com>
2023-12-10 22:16:09 -0500

    commit all the files

---
+++ lib/cards.ex
@@ -0,0 +1,62 @@
+defmodule Cards do
+  @moduledoc """
+  Provides methods for creating and handling a deck of cards.
+  """
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace"
+    ]
+
+    suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} ->
+        :erlang.binary_to_term(binary)
+
+      {:error, _reason} ->
+        "There was a problem."
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> shuffle()
+    |> deal(hand_size)
+  end
+end
